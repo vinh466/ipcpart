46af5693575043563a2bf47d58e71c58030efab6
Vinh Nguye <vinh466@gmail.com>
2022-11-26 15:12:14 +0700

    feat(frontend): edit home page

--- frontend/src/views/home/components/ProductSection.vue
+++ frontend/src/views/home/components/ProductSection.vue
@@ -34,269 +34,280 @@
                 @onViewProduct="handleViewProduct"
                 @onPurchase="handlePurchase"
                 @onAddToCart="handleAddToCart"
-                @onPreview="handlePreview"
               />
             </div>
           </div>
         </div>
       </div>
       <div class="right" :class="banner ? '' : 'no-banner'">
-        <img src="/public/image/product/ssd/banner/ssd-banner-1.png" alt="1" />
-        <img src="/public/image/product/ssd/banner/ssd-banner-2.png" alt="2" />
+        <img
+          v-for="(imageUrl, index) in banner"
+          :key="index"
+          :src="imageUrl"
+          :alt="imageUrl"
+        />
       </div>
     </div>
   </section>
 </template>
 
 <script lang="ts" setup>
-import { getSSD } from '@/api/products/fetchData';
-import type {
-  Product,
-  ProductPageResult,
-  ProductSection,
-} from '@/types/api/products/product';
-import { computed, onMounted, ref, watch } from '@vue/runtime-core';
-import type { ComponentPublicInstance } from 'vue';
-import { useToast } from 'vue-toastification';
-import ProductCardItem from '@/components/ProductCardItem.vue';
-const props = withDefaults(
-  defineProps<{
-    list?: ProductPageResult;
-    sectionTitle?: string;
-    sectionCategory?: Array<string>;
-    reverseCol?: boolean;
-    banner?: boolean;
-    row?: 1 | 2;
-  }>(),
-  // default value:
-  {
-    sectionTitle: 'Section Title',
-    reverseCol: false,
-    banner: true,
-    row: 2,
-  }
-);
-
-const ssd = ref<ProductPageResult>();
-const productListRef = ref<ComponentPublicInstance<HTMLDivElement>>();
-const toast = useToast();
-
-const lengthIndexSrcoll = ref(0);
-const lengthProductRow = ref(0);
-let isDown = false;
-let isDrag = false;
-let startX: number;
-let scrollLeft: number;
-let walk: number;
-let scroll: number;
-let scrollIndex = 0;
-
-onMounted(() => {
-  // ssd.value = getSSD();
-  setTimeout(() => (ssd.value = getSSD()), 10);
-  // console.log(ssd);
-});
-
-const sectionProductData = computed(() => {
-  let productList = ssd.value?.list;
-  if (productList) {
-    let newList: ProductSection = [[], []];
-    lengthProductRow.value = productList.length / props.row;
-
-    let x = 0;
-    for (let index = 0; index < productList.length; index++) {
-      newList[x]?.push(productList[index]);
-      x = x == 0 ? props.row - 1 : 0;
+  import { getSSD } from '@/api/products/fetchData';
+  import type {
+    Product,
+    ProductPageResult,
+    ProductSection,
+  } from '@/types/api/products/product';
+  import { computed, onMounted, ref, watch } from '@vue/runtime-core';
+  import type { ComponentPublicInstance } from 'vue';
+  import { useToast } from 'vue-toastification';
+  import ProductCardItem from '@/components/ProductCardItem.vue';
+  import { useRouter } from 'vue-router';
+  const router = useRouter();
+  const props = withDefaults(
+    defineProps<{
+      list: ProductPageResult;
+      sectionTitle?: string;
+      sectionCategory?: Array<string>;
+      reverseCol?: boolean;
+      banner?: Array<string>;
+      row?: 1 | 2;
+    }>(),
+    // default value:
+    {
+      sectionTitle: 'Section Title',
+      reverseCol: false,
+      row: 2,
     }
+  );
 
-    // console.log(newList);
-    return newList;
+  const ssd = ref<ProductPageResult>();
+  const productListRef = ref<ComponentPublicInstance<HTMLDivElement>>();
+  const toast = useToast();
+
+  const lengthIndexSrcoll = ref(0);
+  const lengthProductRow = ref(0);
+  let isDown = false;
+  let isDrag = false;
+  let startX: number;
+  let scrollLeft: number;
+  let walk: number;
+  let scroll: number;
+  let scrollIndex = 0;
+
+  onMounted(() => {
+    // ssd.value = getSSD();
+    // setTimeout(() => (ssd.value = getSSD()), 10);
+    // console.log(ssd);
+  });
+
+  const sectionProductData = computed(() => {
+    let productList = props.list?.data;
+    if (productList) {
+      let newList: ProductSection = [[], []];
+      lengthProductRow.value = productList.length / props.row;
+
+      let x = 0;
+      for (let index = 0; index < productList.length; index++) {
+        newList[x]?.push(productList[index]);
+        x = x == 0 ? props.row - 1 : 0;
+      }
+
+      console.log(newList);
+      return newList;
+    }
+    return [];
+  });
+
+  function handleViewProduct(id: String, type: string) {
+    if (isDrag) return;
+    let path = `/product/${type.replace(' ', '-')}/${id}`;
+    router.push({ path: path });
   }
-  return [];
-});
 
-function handleViewProduct(e: Event, id: String) {
-  if (isDrag) return;
-  toast('ViewProduct ' + id);
-}
-
-function handlePurchase(e: Event, id: String) {
-  if (isDrag) return;
-  toast('Purchase ' + id);
-}
-
-function handleAddToCart(e: Event, id: String) {
-  if (isDrag) return;
-  toast('AddToCart ' + id);
-}
-
-function handlePreview(e: Event, id: String) {
-  if (isDrag) return;
-  toast('Preview ' + id);
-}
-watch(productListRef, (productListRef) => {
-  if (productListRef) {
-    productListRef.addEventListener(
-      'mousedown',
-      (e) => {
-        isDown = true;
-        productListRef.classList.add('active');
-        startX = e.pageX - (productListRef.offsetLeft || 0);
-        scrollLeft = productListRef.scrollLeft || 0;
-        scroll = 0;
-      },
-      { passive: false }
-    );
-
-    productListRef.addEventListener('mouseleave', () => {
-      if (!isDown) return;
-      isDown = false;
-      productListRef.classList.remove('active');
-      calcSrcollIndex();
-      moveSrcoll(productListRef);
-    });
-
-    productListRef.addEventListener('mouseup', () => {
-      isDown = false;
-      productListRef.classList.remove('active');
-      calcSrcollIndex();
-      moveSrcoll(productListRef);
-    });
-
-    productListRef.addEventListener('mousemove', (e) => {
-      isDrag = false;
-      if (!isDown) return;
-      e.preventDefault();
-      isDrag = true;
-      const x = e.pageX - (productListRef.offsetLeft || 0);
-      walk = (x - startX) * 1; //scroll-fast
-      scroll = scrollLeft - walk;
-      productListRef.scrollLeft = scroll;
-    });
+  function handlePurchase(id: String, type: string) {
+    if (isDrag) return;
+    toast('Purchase ' + type + ', ' + id);
   }
-});
 
-function handleNavigation(action: 'left' | 'right') {
-  if (productListRef?.value) {
-    scrollLeft = productListRef.value.scrollLeft || 0;
-    scrollIndex += action === 'left' ? -1 : 1;
-    moveSrcoll(productListRef?.value);
+  function handleAddToCart(id: String, name: string, type: string) {
+    if (isDrag) return;
+    toast.success('Đã thêm ' + name);
   }
-}
 
-function calcSrcollIndex() {
-  if (!scroll) return;
-  if (scrollIndex > scroll / 240 + 0.2) {
-    scrollIndex = (scroll / 240) >> 0;
-  } else {
+  watch(productListRef, (productListRef) => {
+    if (productListRef) {
+      productListRef.addEventListener(
+        'mousedown',
+        (e) => {
+          isDown = true;
+          productListRef.classList.add('active');
+          startX = e.pageX - (productListRef.offsetLeft || 0);
+          scrollLeft = productListRef.scrollLeft || 0;
+          scroll = 0;
+        },
+        { passive: false }
+      );
+
+      productListRef.addEventListener('mouseleave', () => {
+        if (!isDown) return;
+        isDown = false;
+        productListRef.classList.remove('active');
+        calcSrcollIndex();
+        moveSrcoll(productListRef);
+      });
+
+      productListRef.addEventListener('mouseup', () => {
+        isDown = false;
+        productListRef.classList.remove('active');
+        calcSrcollIndex();
+        moveSrcoll(productListRef);
+      });
+
+      productListRef.addEventListener('mousemove', (e) => {
+        isDrag = false;
+        if (!isDown) return;
+        e.preventDefault();
+        isDrag = true;
+        const x = e.pageX - (productListRef.offsetLeft || 0);
+        walk = (x - startX) * 1; //scroll-fast
+        scroll = scrollLeft - walk;
+        productListRef.scrollLeft = scroll;
+      });
+    }
+  });
+
+  function handleNavigation(action: 'left' | 'right') {
+    if (productListRef?.value) {
+      scrollLeft = productListRef.value.scrollLeft || 0;
+      scrollIndex += action === 'left' ? -1 : 1;
+      moveSrcoll(productListRef?.value);
+    }
+  }
+
+  function calcSrcollIndex() {
+    if (!scroll) return;
+    if (scrollIndex > scroll / 240 + 0.2) {
+      scrollIndex = (scroll / 240) >> 0;
+    } else {
+      scrollIndex =
+        scroll / 240 - ((scroll / 240) >> 0) > 0.3
+          ? ((scroll / 240) >> 0) + 1
+          : (scroll / 240) >> 0;
+    }
+  }
+
+  function moveSrcoll(element: HTMLDivElement) {
+    lengthIndexSrcoll.value =
+      lengthProductRow.value - element.offsetWidth / 240;
     scrollIndex =
-      scroll / 240 - ((scroll / 240) >> 0) > 0.3
-        ? ((scroll / 240) >> 0) + 1
-        : (scroll / 240) >> 0;
+      scrollIndex < 0
+        ? 0
+        : scrollIndex > lengthIndexSrcoll.value
+        ? (lengthIndexSrcoll.value >> 0) + 1
+        : scrollIndex;
+    element.style.scrollBehavior = 'smooth';
+    element.scrollLeft = 240 * scrollIndex;
+    element.style.scrollBehavior = 'unset';
   }
-}
-
-function moveSrcoll(element: HTMLDivElement) {
-  lengthIndexSrcoll.value = lengthProductRow.value - element.offsetWidth / 240;
-  scrollIndex =
-    scrollIndex < 0
-      ? 0
-      : scrollIndex > lengthIndexSrcoll.value
-      ? (lengthIndexSrcoll.value >> 0) + 1
-      : scrollIndex;
-  element.style.scrollBehavior = 'smooth';
-  element.scrollLeft = 240 * scrollIndex;
-  element.style.scrollBehavior = 'unset';
-}
 </script>
 
 <style lang="scss" scoped>
-.section-product {
-  .section-top {
-    display: flex;
-    justify-content: space-between;
-    border-bottom: 5px solid var(--color-primary);
-
-    &__left {
+  .section-product {
+    .section-top {
       display: flex;
-      align-items: center;
+      justify-content: space-between;
+      border-bottom: 5px solid var(--color-primary);
 
-      .heading {
-        font-size: 24px;
-        font-weight: 700;
-        color: white;
-        padding: 0 10px;
-        border-radius: 5px 5px 0 0;
-        background-color: var(--color-primary);
-      }
+      &__left {
+        display: flex;
+        align-items: center;
 
-      .navigation {
-        margin-left: 12px;
-        height: 100%;
-        button {
-          background-color: transparent;
+        .heading {
+          font-size: 24px;
+          font-weight: 700;
+          color: white;
+          padding: 0 10px;
+          border-radius: 5px 5px 0 0;
+          background-color: var(--color-primary);
+        }
+
+        .navigation {
+          margin-left: 12px;
           height: 100%;
-          font-size: 1.2rem;
-          color: var(--color-primary);
 
-          &:hover {
-            background-color: var(--color-border-hover);
-          }
-        }
-      }
-    }
-    &__right {
-      & > * {
-        margin-left: 5px;
-      }
-    }
-  }
-  .reverse-col {
-    flex-direction: row-reverse;
-  }
-  .section-main {
-    display: flex;
-    margin: 10px 0;
-    .left {
-      overflow: hidden;
+          button {
+            background-color: transparent;
+            height: 100%;
+            font-size: 1.2rem;
+            color: var(--color-primary);
 
-      .product-list {
-        overflow: hidden;
-
-        & > * {
-          margin-right: 12px;
-        }
-
-        .product-cart {
-          margin: 15px 0;
-        }
-        .product-list-row {
-          display: flex;
-          margin-bottom: 10px;
-
-          & > * {
-            margin: 0 5px;
-
-            & > .card {
-              width: 220px;
-              height: 100%;
-              margin: 0;
+            &:hover {
+              background-color: var(--color-border-hover);
             }
           }
         }
       }
+
+      &__right {
+        & > * {
+          margin-left: 5px;
+        }
+      }
     }
 
-    .right {
-      min-width: 260px;
-      margin: 0 10px;
-      img {
-        width: 100%;
+    .reverse-col {
+      flex-direction: row-reverse;
+    }
+
+    .section-main {
+      display: flex;
+      margin: 10px 0;
+
+      .left {
+        overflow: hidden;
+
+        .product-list {
+          overflow: hidden;
+
+          & > * {
+            margin-right: 12px;
+          }
+
+          .product-cart {
+            margin: 15px 0;
+          }
+
+          .product-list-row {
+            display: flex;
+            margin-bottom: 10px;
+
+            & > * {
+              margin: 0 5px;
+
+              & > .card {
+                width: 220px;
+                height: 100%;
+                margin: 0;
+              }
+            }
+          }
+        }
       }
-      &.no-banner {
-        display: none;
+
+      .right {
+        min-width: 260px;
+        width: 280px;
+        margin: 0 10px;
+
+        img {
+          width: 100%;
+        }
+
+        &.no-banner {
+          display: none;
+        }
       }
     }
   }
-}
 </style>
---
+++ frontend/src/views/home/home.vue
@@ -0,0 +1,184 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <div class="banner">
+        <carousel :autoplay="6000" :wrap-around="true">
+          <slide v-for="imgUrl in slidesImg" :key="imgUrl">
+            <div class="carousel__item">
+              <img :src="`${imgUrl}`" :alt="imgUrl.slice(0, -4)" />
+            </div>
+          </slide>
+
+          <template #addons>
+            <navigation />
+            <pagination />
+          </template>
+        </carousel>
+      </div>
+
+      <div>
+        <!-- <ProductSection
+        v-if="cpuData" :list="cpuData" 
+          sectionTitle="Dành cho bạn"
+          :sectionCategory="['asd', 'asdsad']"
+        /> -->
+        <div v-if="cpuData">
+          <ProductSection
+            section-title="CPU"
+            :banner="banner.cpu"
+            :list="cpuData"
+            :reverse-col="true"
+          />
+        </div>
+        <div v-if="mainboardData">
+          <ProductSection
+            section-title="Mainboard"
+            :banner="banner.mainboard"
+            :list="mainboardData"
+          />
+        </div>
+        <div v-if="ramData">
+          <ProductSection
+            section-title="Ram"
+            :banner="banner.ram"
+            :list="ramData"
+            :reverse-col="true"
+          />
+        </div>
+        <div v-if="monitorData">
+          <ProductSection
+            section-title="Monitor"
+            :banner="banner.monitor"
+            :list="monitorData"
+          />
+        </div>
+        <div v-if="mouseData">
+          <ProductSection
+            section-title="Mouse"
+            :banner="banner.mouse"
+            :list="mouseData"
+            :reverse-col="true"
+          />
+        </div>
+        <div v-if="storagedriveData">
+          <ProductSection
+            section-title="Storage Drive"
+            :banner="banner.storagedrive"
+            :list="storagedriveData"
+          />
+        </div>
+        <div v-if="videocardData">
+          <ProductSection
+            section-title="Video Card"
+            :banner="banner.videocard"
+            :list="videocardData"
+            :reverse-col="true"
+          />
+        </div>
+        <!-- <ProductSection :reverse-col="true" :banner="false" :row="2" /> -->
+      </div>
+
+      <div>
+        <h1>Last View Section</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { Carousel, Pagination, Slide } from 'vue3-carousel';
+  import productService from '@/service/products/product.service';
+  import type { ProductPageResult } from '@/types/api/products/product';
+  import { onMounted, ref } from '@vue/runtime-core';
+  import CardList from '../../components/ProductCardList.vue';
+  import ProductSection from './components/ProductSection.vue';
+  const cpuData = ref<ProductPageResult>();
+  const mainboardData = ref<ProductPageResult>();
+  const ramData = ref<ProductPageResult>();
+  const monitorData = ref<ProductPageResult>();
+  const mouseData = ref<ProductPageResult>();
+  const storagedriveData = ref<ProductPageResult>();
+  const videocardData = ref<ProductPageResult>();
+  const banner = {
+    storagedrive: [
+      '/public/image/product/banner/storagedrive-banner-1.png',
+      '/public/image/product/banner/storagedrive-banner-2.png',
+    ],
+    ram: [
+      '/public/image/product/banner/ram-banner-1.png',
+      '/public/image/product/banner/ram-banner-2.png',
+    ],
+    cpu: [
+      '/public/image/product/banner/cpu-banner-1.png',
+      '/public/image/product/banner/cpu-banner-2.png',
+    ],
+    mainboard: [
+      '/public/image/product/banner/mainboard-banner-1.png',
+      '/public/image/product/banner/mainboard-banner-2.png',
+    ],
+    videocard: [
+      '/public/image/product/banner/videocard-banner-1.png',
+      '/public/image/product/banner/videocard-banner-2.png',
+    ],
+    mouse: [
+      '/public/image/product/banner/mouse-banner-1.png',
+      '/public/image/product/banner/mouse-banner-2.png',
+    ],
+    monitor: [
+      '/public/image/product/banner/monitor-banner-1.png',
+      '/public/image/product/banner/monitor-banner-2.png',
+    ],
+  };
+  const slidesImg = [
+    'public/image/slices/slide-1.png',
+    'public/image/slices/slide-7.png',
+    'public/image/slices/slide-3.png',
+    'public/image/slices/slide-4.png',
+    'public/image/slices/slide-5.png',
+    'public/image/slices/slide-6.png',
+  ];
+  onMounted(async () => {
+    cpuData.value = await productService.getProducts('cpu');
+    mainboardData.value = await productService.getProducts('mainboard');
+    ramData.value = await productService.getProducts('ram');
+    monitorData.value = await productService.getProducts('monitor');
+    mouseData.value = await productService.getProducts('mouse');
+    storagedriveData.value = await productService.getProducts('storage drive');
+    videocardData.value = await productService.getProducts('video card');
+  });
+</script>
+
+<style lang="scss" scoped>
+  .banner {
+    img {
+      width: 100%;
+      // height: 430px;
+      margin: 15px 0;
+      object-fit: cover;
+      background: white;
+      border-radius: 8px;
+    }
+
+    .carousel__item {
+      min-height: 200px;
+      width: 100%;
+      color: var(--vc-clr-white);
+      font-size: 20px;
+      display: flex;
+      overflow: hidden;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .carousel__pagination {
+      display: none;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .home {
+      display: flex;
+      align-items: center;
+    }
+  }
+</style>
